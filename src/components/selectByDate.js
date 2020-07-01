86644144a6154ce7346c74024c360a11c8537c24
CherryChann <cherrychrishateu@gmail.com>
2020-07-01 20:52:54 +0630

    Validated for date range(tomorrow-till 1 week)

--- src/components/selectByDate.js
+++ src/components/selectByDate.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Row, Col} from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
-
 import "react-datepicker/dist/react-datepicker.css";
 
-const selectByDate = ({startDate,onChange}) => (
+const selectByDate = ({startDate,minDate, maxDate, onChange}) => (
     <Row>
         <Col lg="2" xs="12">
             <span>Date: </span>
-            
         </Col>
         <Col lg="3" xs="12" className="form-group">
             <DatePicker
                 className="custom-date-picker"
                 selected={startDate}
+                minDate={minDate}
+                maxDate={maxDate}
+
                 onChange = {
                     date => onChange(date)
                 }
--- src/features/home/component/index.js
+++ src/features/home/component/index.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { Container,Row, Col, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
+
 import NavBar from '../../../components/navBar';
 import SelectByProduct from '../../../components/selectByProduct';
 import SelectByDate from '../../../components/selectByDate';
 import LoadingProduct from '../../../components/loading';
 import '../../../custom.scss';
 import { getProductsIfNeeded } from '../../../services/productService';
+import { addDays } from 'date-fns';
 
 class HomePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            startDate : new Date(),
+            startDate: addDays(new Date(), 1),
+            minDate: addDays(new Date(), 1),
+            maxDate: addDays(new Date(),7),
             selectedProduct: {}
         }
     }
@@ -25,6 +29,7 @@ class HomePage extends Component {
         selectedProduct: product
     });
     componentDidMount () {
+        console.log(this.state.startDate, this.state.minDate, 'date')
         const { dispatch } = this.props;
         dispatch(getProductsIfNeeded()); // fetch products list from api
     }
@@ -38,7 +43,7 @@ class HomePage extends Component {
                     <LoadingProduct/> : 
                     <SelectByProduct products={this.props.products} onClick={this.getSelectedProduct}/>
                     }
-                    <SelectByDate startDate={this.state.startDate} onChange={this.handleChange} />
+                    <SelectByDate startDate={this.state.startDate} minDate={this.state.minDate} maxDate={this.state.maxDate} onChange={this.handleChange} />
                     <Row>
                         <Col lg="2" xs="12">
                             <span>Locations: </span>
